3fe32c7c6909e19494efa8cb9defbfc65ebb979d
Cristian Lahoz <108961907+m415x@users.noreply.github.com>
2025-04-15 12:10:09 -0300

    Update integrador-1.py

--- integrador-1.py
+++ integrador-1.py
@@ -1,1 +1,22 @@
+# ===== CONTADOR BINARIO =====
+# Escriban un programa que, usando un ciclo, cuente desde 0 hasta 15 y muestre
+# cada número en su representación binaria.
+# Extensión: Utilicen un retardo (por ejemplo, con time.sleep) para simular el conteo
+# de un circuito.
+
+
+# ===== Importaciones de módulos =====
+# Módulo time.sleep(seg)
+# Suspende la ejecución del hilo que lo invoca por el número de segundos dado. El argumento 
+# puede ser un número de punto flotante para indicar un tiempo de suspensión más preciso. 
+# El tiempo de suspensión real puede ser menor que el solicitado porque cualquier señal 
+# detectada terminará la función sleep() siguiendo la rutina de captura de la señal. El 
+# tiempo de suspensión también puede ser más largo que el solicitado por una cantidad 
+# arbitraria debido a la planificación de otra actividad en el sistema.
+
+
+# ===== Definición de funciones =====
+
+
+# ===== Programa principal =====
 
