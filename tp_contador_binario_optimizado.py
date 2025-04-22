2c1902231628049c3a4ddf863eb0683b0ce3ff05
Ariana Maldonado <155493018+AriMaldo19@users.noreply.github.com>
2025-04-22 15:25:32 -0300

    Update tp_contador_binario_optimizado.py

--- tp_contador_binario_optimizado.py
+++ tp_contador_binario_optimizado.py
@@ -4,18 +4,18 @@ def contador_binario():
     # preguntar al usuario si quiere ver los números en orden ascendente o descendente.
     orden = input("¿Quieres ver los números en orden ascendente o descendente? (asc/desc): ").lower()
 
-    if  orden == 'asc':              # si el usuario elige 'asc', contamos de 0 a 15.
+    if  orden == 'asc':                  # si el usuario elige 'asc', contamos de 0 a 15.
         rango = range(16)
-    elif orden == 'desc':            # si el usuario elige 'desc', contamos de 15 a 0.
+    elif orden == 'desc':                # si el usuario elige 'desc', contamos de 15 a 0.
         rango = range(15, -1, -1)
-    else:                            # si el usuario no elige 'asc' o 'desc', mostramos mensaje de "error".
+    else:                                # si el usuario no elige 'asc' o 'desc', mostramos mensaje de "error".
         print("Error, define un orden correcto por favor ('asc' o 'desc').")
-        return                       # salir de la función. 
+        return                           # salir de la función. 
             
-    for numero in rango:             # bucle for: Itera sobre el rango elegido por el usuario.
-        binario = bin(numero)[2:]    # convierte el número a binario y elimina '0b'.
-        binario = binario.zfill(4)   # rellena con ceros a la izquierda para 4 cifras.
-        print(f"{binario} = {numero}")  # muestra el número binario seguido de su equivalente decimal.
-        time.sleep(1)                # pausa de 1 segundo entre cada impresión, para mostrar cada número con intervalo.
+    for numero in rango:                 # bucle for: Itera sobre el rango elegido por el usuario.
+        binario = bin(numero)[2:]        # convierte el número a binario y elimina '0b'.
+        binario = binario.zfill(4)       # rellena con ceros a la izquierda para 4 cifras.
+        print(f"{binario} = {numero}")   # muestra el número binario seguido de su equivalente decimal.
+        time.sleep(1)                    # pausa de 1 segundo entre cada impresión, para mostrar cada número con intervalo.
 
-contador_binario()                   # llamada a la función para ejecutar el contador.
+contador_binario()                       # llamada a la función para ejecutar el contador.
